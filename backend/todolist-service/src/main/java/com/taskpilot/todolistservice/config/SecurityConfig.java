d4087322fba186d388b970131769986d9dcba571
Joseph Hit Hard <joseph.meghanath@gmail.com>
2025-07-25 16:59:54 -0500

    fixed health api for task service

--- backend/todolist-service/src/main/java/com/taskpilot/todolistservice/config/SecurityConfig.java
+++ backend/todolist-service/src/main/java/com/taskpilot/todolistservice/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "http://127.0.0.1:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173", "https://jdtaskpilot.netlify.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
